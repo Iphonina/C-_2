9c2ad4a0794f3809633576e50df0670d13fd20a6
Yana <leduschenkova6@gmail.com>
2022-09-20 09:46:31 +0300

    add Task 3

---
+++ Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿// На входе 2 числа и выводить, является ли второе число кратным первому. 
+// Если число 2 не кратно числу 1, то программа выводит остаток от деления.
+// 34, 5 -> не кратно, остаток 4 
+// 16, 4 -> кратно
+
+Console.Write("Введите первое число ");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число ");
+int numB = Convert.ToInt32(Console.ReadLine());
+
+if (numA % numB == 0) Console.Write("кратно");
+else Console.Write($"не кратно, остаток {numA % numB}");
