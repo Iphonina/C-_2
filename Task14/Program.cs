3b4c7b3f70c78f110a93ff1a9a1e6238a3e6869d
Yana <leduschenkova6@gmail.com>
2022-09-20 09:47:33 +0300

    add Task 14

---
+++ Task14/Program.cs
@@ -0,0 +1,6 @@
+﻿// Задача 14. На вход число и проверяет кратно ли оно одновременно 7 и 23
+//int n = new Random().Next();
+//Console.WriteLine(n);
+int n = 320;
+if (n % 7 == 0 && n % 23 == 0) Console.WriteLine("да, кратно");
+else Console.WriteLine("нет");
