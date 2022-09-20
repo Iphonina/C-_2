1060ab078f76bd646608ecbd51923d881f8fefbd
Yana <leduschenkova6@gmail.com>
2022-09-20 09:43:58 +0300

    add csharp

---
+++ csharp/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("");
+int n = 10;
+while (n > 3)
+{
+    Console.WriteLine(n);
+    n -= 1;
+}
+
+
+for (int i = 3; i < 10; i++) 
+{
+    int x = new Random().Next(1,100);
+    Console.WriteLine($"Наша переменная {i} равна {x}");
+}
+
