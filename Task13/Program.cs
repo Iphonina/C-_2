769b34677cb9540afdcc603c0efe386a33a4a301
Yana <leduschenkova6@gmail.com>
2022-09-20 09:47:17 +0300

    add Task 13

---
+++ Task13/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача13. Выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.Write("Введите число ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+if (x <= 99) Console.Write("третьей цифры нет");
+else if (x < 1000) Console.Write(x%10);
+else 
+{
+    while(x >= 1000)
+    {
+        x/=10;
+    }
+    Console.WriteLine(x%10);
+}
+    
+
+
+
+
