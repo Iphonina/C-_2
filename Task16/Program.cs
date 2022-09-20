1d3ed8c909fde95bad6e7cd462a526e3284ec360
Yana <leduschenkova6@gmail.com>
2022-09-20 09:48:04 +0300

    add Task 16

---
+++ Task16/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напиши программу, где вводим три числа, а потом выводим сумму только положительных чисел.
+Console.Write("Введите первое число ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число ");
+int n3 = Convert.ToInt32(Console.ReadLine());
+
+int sum = 0;
+
+if (n1 >= 0) sum = sum + n1;
+if (n2 >= 0) sum = sum + n2;
+if (n3 >= 0) sum = sum + n3;
+
+Console.WriteLine(sum);
